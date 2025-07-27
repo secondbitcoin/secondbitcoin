32aea7d1227a29d2fae38ebde5330f09751b660a
x <stamina_61steps@icloud.com>
2025-07-27 01:52:23 -0700

    Change port from 8333 to 9333

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -105,7 +105,7 @@ public:
         pchMessageStart[2] = 0xb4;
         pchMessageStart[3] = 0xd8;
         vAlertPubKey = ParseHex("04fc9702847840aaf195de8442ebecedf5b095cdbb9bc716bda9110971b28a49e0ead8564ff0db22209e0374782c093bb899692d524e9d6a6956e7c5ecbcd68284");
-        nDefaultPort = 8333;
+        nDefaultPort = 9333;
         nMaxTipAge = 24 * 60 * 60;
         nPruneAfterHeight = 100000;
 
