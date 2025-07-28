dd60e5acab852f9ca40085f58c06e89f5ea5b276
X <stamina_61steps@icloud.com>
2025-07-29 00:55:09 +1200

    add seed

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -144,7 +144,7 @@ public:
         // This is fine at runtime as we'll fall back to using them as an addrfetch if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("dummySeed.invalid.");
+        vSeeds.emplace_back("seed.secondbitcoin.com.");
         /*
 	vSeeds.emplace_back("seed.bitcoin.sipa.be."); // Pieter Wuille, only supports x1, x5, x9, and xd
         vSeeds.emplace_back("dnsseed.bluematt.me."); // Matt Corallo, only supports x9
