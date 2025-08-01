b4743e27e99bda86eb1d12d5087d2be304bd4d2f
x <stamina_61steps@icloud.com>
2025-07-31 23:39:47 -0700

    clear bitcoin fixed seeds

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -124,6 +124,7 @@ public:
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
+        vFixedSeeds.clear();
 
         fMiningRequiresPeers = true;
         fDefaultConsistencyChecks = false;
